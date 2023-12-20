788843dc64ab796c67b110e8e53dc0ebef9cf43a
lishuangqiang <142634204+lishuangqiang@users.noreply.github.com>
2023-12-20 23:59:02 +0800

    Initial commit

---
+++ src/main/java/com/yupi/springbootinit/controller/ChartController.java
@@ -0,0 +1,322 @@
+package com.yupi.springbootinit.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.google.gson.Gson;
+import com.yupi.springbootinit.annotation.AuthCheck;
+import com.yupi.springbootinit.common.BaseResponse;
+import com.yupi.springbootinit.common.DeleteRequest;
+import com.yupi.springbootinit.common.ErrorCode;
+import com.yupi.springbootinit.common.ResultUtils;
+import com.yupi.springbootinit.constant.CommonConstant;
+import com.yupi.springbootinit.constant.PromptConstant;
+import com.yupi.springbootinit.constant.UserConstant;
+import com.yupi.springbootinit.exception.BusinessException;
+import com.yupi.springbootinit.exception.ThrowUtils;
+import com.yupi.springbootinit.manager.AImanager;
+import com.yupi.springbootinit.model.dto.chart.*;
+import com.yupi.springbootinit.model.entity.Chart;
+import com.yupi.springbootinit.model.entity.User;
+import com.yupi.springbootinit.model.vo.Biresponsed;
+import com.yupi.springbootinit.service.ChartService;
+import com.yupi.springbootinit.service.UserService;
+import com.yupi.springbootinit.utils.ExcleUtils;
+import com.yupi.springbootinit.utils.SqlUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 帖子接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/chart")
+@Slf4j
+public class ChartController {
+
+    @Resource
+    private ChartService chartService;
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private AImanager aImanager;
+
+
+    private final static Gson GSON = new Gson();
+
+    // region 增删改查
+
+    /**
+     * 创建
+     *
+     * @param chartAddRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/add")
+    public BaseResponse<Long> addChart(@RequestBody ChartAddRequest chartAddRequest, HttpServletRequest request) {
+        if (chartAddRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        /*
+        * 创建一个chat对象，将charAddRequest中的字段填充到chart中
+        * */
+        Chart chart = new Chart();
+        BeanUtils.copyProperties(chartAddRequest, chart);
+        /*
+        * 从数据库中查询当前登录用户，并且设置Chart中图标对应的用户
+        * */
+        User loginUser = userService.getLoginUser(request);
+        chart.setUserId(loginUser.getId());
+        /*
+        * 尝试将char对象保存到数据库当中
+        * */
+        boolean result = chartService.save(chart);
+        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR);
+        long newChartId = chart.getId();
+        return ResultUtils.success(newChartId);
+    }
+
+    /**
+     * 删除
+     *
+     * @param deleteRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/delete")
+    public BaseResponse<Boolean> deleteChart(@RequestBody DeleteRequest deleteRequest, HttpServletRequest request) {
+        if (deleteRequest == null || deleteRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User user = userService.getLoginUser(request);
+        long id = deleteRequest.getId();
+        // 判断是否存在
+        Chart oldChart = chartService.getById(id);
+        ThrowUtils.throwIf(oldChart == null, ErrorCode.NOT_FOUND_ERROR);
+        // 仅本人或管理员可删除
+        if (!oldChart.getUserId().equals(user.getId()) && !userService.isAdmin(request)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        boolean b = chartService.removeById(id);
+        return ResultUtils.success(b);
+    }
+
+    /**
+     * 更新（仅管理员）
+     *
+     * @param chartUpdateRequest
+     * @return
+     */
+    @PostMapping("/update")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Boolean> updateChart(@RequestBody ChartUpdateRequest chartUpdateRequest) {
+        if (chartUpdateRequest == null || chartUpdateRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Chart chart = new Chart();
+        BeanUtils.copyProperties(chartUpdateRequest, chart);
+
+        // 参数校验
+        long id = chartUpdateRequest.getId();
+        // 判断是否存在
+        Chart oldChart = chartService.getById(id);
+        ThrowUtils.throwIf(oldChart == null, ErrorCode.NOT_FOUND_ERROR);
+        boolean result = chartService.updateById(chart);
+        return ResultUtils.success(result);
+    }
+
+    /**
+     * 根据 id 获取
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/get")
+    public BaseResponse<Chart> getChartById(long id, HttpServletRequest request) {
+        if (id <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Chart chart = chartService.getById(id);
+        if (chart == null) {
+            throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
+        }
+        return ResultUtils.success(chart);
+    }
+
+    /**
+     * 分页获取列表（封装类）
+     *
+     * @param chartQueryRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/list/page")
+    public BaseResponse<Page<Chart>> listChartByPage(@RequestBody ChartQueryRequest chartQueryRequest,
+            HttpServletRequest request) {
+        long current = chartQueryRequest.getCurrent();
+        long size = chartQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        Page<Chart> chartPage = chartService.page(new Page<>(current, size),
+                getQueryWrapper(chartQueryRequest));
+        return ResultUtils.success(chartPage);
+    }
+
+    /**
+     * 分页获取当前用户创建的资源列表
+     *
+     * @param chartQueryRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/my/list/page")
+    public BaseResponse<Page<Chart>> listMyChartByPage(@RequestBody ChartQueryRequest chartQueryRequest,
+            HttpServletRequest request) {
+        if (chartQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        chartQueryRequest.setUserId(loginUser.getId());
+        long current = chartQueryRequest.getCurrent();
+        long size = chartQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        Page<Chart> chartPage = chartService.page(new Page<>(current, size),
+              getQueryWrapper(chartQueryRequest));
+        return ResultUtils.success(chartPage);
+    }
+
+    // endregion
+
+
+
+    /**
+     * 编辑（用户）
+     *
+     * @param chartEditRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/edit")
+    public BaseResponse<Boolean> editChart(@RequestBody ChartEditRequest chartEditRequest, HttpServletRequest request) {
+        if (chartEditRequest == null || chartEditRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Chart chart = new Chart();
+        BeanUtils.copyProperties(chartEditRequest, chart);
+
+
+
+        User loginUser = userService.getLoginUser(request);
+        long id = chartEditRequest.getId();
+        // 判断是否存在
+        Chart oldChart = chartService.getById(id);
+        ThrowUtils.throwIf(oldChart == null, ErrorCode.NOT_FOUND_ERROR);
+        // 仅本人或管理员可编辑
+        if (!oldChart.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        boolean result = chartService.updateById(chart);
+        return ResultUtils.success(result);
+    }
+    /**
+     * 智能分析
+     *
+     * @param multipartFile
+     * @param genChartByAiRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/gen")
+    public BaseResponse<Biresponsed> genChartByAi(@RequestPart("file") MultipartFile multipartFile,
+                                             GenChartByAiRequest genChartByAiRequest, HttpServletRequest request) {
+        String name = genChartByAiRequest.getName();
+        String goal = genChartByAiRequest.getGoal();
+        String chartType = genChartByAiRequest.getChartType();
+        //校验
+        ThrowUtils.throwIf(StringUtils.isBlank(name), ErrorCode.PARAMS_ERROR,"名称为空");
+        ThrowUtils.throwIf(StringUtils.isNotBlank(name)&&name.length()>100,ErrorCode.PARAMS_ERROR,"名称过长");
+        ThrowUtils.throwIf(StringUtils.isBlank(goal), ErrorCode.PARAMS_ERROR,"目标为空");
+        ThrowUtils.throwIf(StringUtils.isBlank(chartType), ErrorCode.PARAMS_ERROR,"图表类型为空");
+
+        //用户输入
+        StringBuilder userInput = new StringBuilder();
+        //拼接内容：
+        userInput.append(PromptConstant.Ai_Promot).append("\n");
+        userInput.append("分析需求:").append(goal).append("\n");
+        userInput.append("使用图表类型:").append(chartType).append("\n");
+        //压缩后的数据
+        //为了进行数据压缩，需要将其转变成为CSV的格式
+        String result = ExcleUtils.excleToCsv(multipartFile);
+        userInput.append("原始数据:").append("\n").append(result).append("\n");
+
+        //将数据交由AI进行处理
+        String resultByAi = aImanager.doChat(userInput.toString());
+
+        String[] split = resultByAi.split("【【【");
+        if(split.length<3)
+        {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR,"生成格式错误，请重新尝试");
+        }
+        //拆分AI结果
+        String analyzeCode = split[1];
+        String analyzeResult = split[2];
+        //插入到数据库
+        Chart chart = new Chart();
+        BeanUtils.copyProperties(genChartByAiRequest, chart);
+        chart.setChartData(result);
+        chart.setName(name);
+        chart.setGoal(goal);
+        chart.setChartType(chartType);
+        chart.setGenResult(analyzeResult);
+        chart.setGenChart(analyzeCode);
+        chartService.saveOrUpdate(chart);
+        //拆分返回结果
+        Biresponsed biresponsed = new Biresponsed();
+        biresponsed.setGenChart(analyzeCode);
+        biresponsed.setGenResult(analyzeResult);
+        return ResultUtils.success(biresponsed);
+    }
+    /**
+     * 查询获取包装类
+     * @param chartQueryRequest
+     * @return
+     */
+
+    private QueryWrapper<Chart> getQueryWrapper(ChartQueryRequest chartQueryRequest) {
+        QueryWrapper<Chart> queryWrapper = new QueryWrapper<>();
+        if (chartQueryRequest == null) {
+            return queryWrapper;
+        }
+        String name = chartQueryRequest.getName();
+        Long id = chartQueryRequest.getId();
+        String goal = chartQueryRequest.getGoal();
+        String chartType = chartQueryRequest.getChartType();
+        Long userId = chartQueryRequest.getUserId();
+        String sortField = chartQueryRequest.getSortField();
+        String sortOrder = chartQueryRequest.getSortOrder();
+
+        queryWrapper.eq(id!=null && id>0,"id",id);
+        queryWrapper.eq(StringUtils.isNotBlank(goal), "goal", goal);
+        queryWrapper.eq(StringUtils.isNotBlank(chartType), "chart_type", chartType);
+        queryWrapper.eq(ObjectUtils.isNotEmpty(userId), "userId", userId);
+        queryWrapper.like(StringUtils.isNotBlank(name), "name", name);
+
+        queryWrapper.eq("isDelete", false);
+        queryWrapper.orderBy(SqlUtils.validSortField(sortField), sortOrder.equals(CommonConstant.SORT_ORDER_ASC),
+                sortField);
+        return queryWrapper;
+    }
+}
---
+++ src/main/java/com/yupi/springbootinit/model/vo/Biresponsed.java
@@ -0,0 +1,19 @@
+package com.yupi.springbootinit.model.vo;
+
+import lombok.Data;
+
+/*
+* Bi返回接轨
+* */
+@Data
+public class Biresponsed {
+    /*
+    * 图标代码
+    * */
+    private String genChart;
+
+    /*
+    * 分析结果
+    * */
+    private String genResult;
+}
